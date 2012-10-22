38de8d7957efffccbf8d3500c2a5eba677b2205f
joonas erkheikki <erkheikki@hotmail.com>
2012-10-22 16:37:02 -0700

    index.php:sta turhat ominaisuudet pois.

--- public/index.php
+++ public/index.php
@@ -10,12 +10,12 @@
  */
 $root = realpath(dirname(__FILE__));
 define('ROOT', $root);
-//asdasdasdasdasdasdasdasd
+
 // Add secret devel parameter to query string for devel info.
-if(isset($_GET['g04753m135'])) {
+/*if(isset($_GET['g04753m135'])) {
 	phpinfo();
 	die();
-}
+}*/
 
 // Require basic kobros klasses. Modules be using auto load so kobro cms very light!
 require_once ROOT . '/inc/KobroCms.php'; 
@@ -32,16 +32,14 @@ try {
         echo $app->run();
 } catch(Exception $e) {
 		
-	echo "<h1>KobroCMS Fatal Error</h1>";
-	
-	echo "<em>" . $e . "</em>";
+	echo "<h1>KobroCMS</h1>";
 
 	// We kobros developers be very clever: we hide stack trace from customer if not devel mode!
-	if($app->config['mode'] == 'development') {
+	/*if($app->config['mode'] == 'development') {
 		print "<pre>";
 		print_r($e->getTrace());
 		print "</pre>";
-	}
+	}*/
 	
 }
 
