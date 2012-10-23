b27fccf6e22fd04201d281705336787040c16b06
Heikki Alanen <heikki.alanen@metropolia.fi>
2012-10-22 19:59:31 -0700

    Moved config.ini from unsafe public outside web scope

--- public/inc/KobroCms.php
+++ public/inc/KobroCms.php
@@ -48,7 +48,7 @@ class KobroCms
 	private function __construct()
 	{
 		// We parse customers config.
-		$this->config = $config = parse_ini_file(ROOT . "/config.ini");
+		$this->config = $config = parse_ini_file(ROOT . "/../config.ini");
 				
 		// We connect to database
 		$this->db = new PDO("mysql:host={$this->config['db_host']};dbname={$this->config['db_schema']}", $this->config['db_user'], $this->config['db_password']);
