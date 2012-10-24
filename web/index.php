c8e6b1ae5f92c0f84f8d9b543d0181bb3716c175
petri <petri@petri-OptiPlex-740-Enhanced.(none)>
2012-10-24 23:09:07 +0300

    Added URLs to pages. Also added about us -page

--- web/index.php
+++ web/index.php
@@ -9,9 +9,18 @@ $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
-$app->get('/', function() use ($app){
+$app->get('/', function() use ($app) {
     return $app['twig']->render('home.html.twig');
-});
+})
+->bind('home');
+
+$app->get('/about', function() use ($app) {
+    return $app['twig']->render('about.html.twig', array(
+        'title' => 'About Dr. Kobros FoundationSSSI'
+    ));     
+})
+->bind('about');
 
 $app->run();
